be87b3422813d9898e1f380a0d2b4dd51bc63e22
rogerPined <ultricularcord2@hotmail.com>
2016-11-11 15:13:58 +0000

    Tarea3

---
+++ src/Asteroide.java
@@ -0,0 +1,11 @@
+
+public class Asteroide extends CuerpoEstelar  {
+int direccion;
+	
+	Asteroide (int posicion, int masa, int direccion)
+	{
+		super(posicion,masa);
+this.direccion= direccion;
+
+	}
+}
---
+++ src/CuerpoEstelar.java
@@ -0,0 +1,14 @@
+
+public class CuerpoEstelar {
+
+
+	int posicion;
+	int masa;
+	
+	
+	CuerpoEstelar (int masa,int posicion)
+	{
+		this.masa=masa;
+		this.posicion=posicion;
+	}
+}
